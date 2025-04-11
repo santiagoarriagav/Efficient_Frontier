b0a9cf1a42090a7e259c4b6e26f5c664702e6988
santi <santi@desktop.com>
2025-04-11 09:06:00 -0600

    Create model for asset prices

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import AssetPrices
 
 # Register your models here.
+
+@admin.register(AssetPrices)
+class AssetPricesAdmin(admin.ModelAdmin):
+    list_display = ("ticker", "date", "close")
+    list_filter = ("ticker")
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class AssetPrices(models.Model):
+    ticker = models.CharField(max_length=10)
+    date = models.DateField()
+    close = models.FloatField()
+
+    class Meta:
+        unique_together = ("ticker", "date")
+    
+    def __str__(self):
+        return f"{self.ticker} - {self.date}"
