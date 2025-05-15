31ca53122b242749473a3a93c0fa121074ebae53
santi <santi@desktop.com>
2025-05-15 03:52:11 -0600

    Insalled dependencies, continue fxing tailwind bug

--- next-frontend/next.config.ts
+++ next-frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import tailwindcss from '@tailwindcss/postcss'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  plugins: [
+    tailwindcss(),
+  ],
 };
 
 export default nextConfig;
